f2d0da935ff700a502d3e061231c1df10634e1f0
Bestlee94 <wagray84@gmail.com>
2022-07-04 20:21:07 +0900

    AdminLayout 나가기버튼구현

--- src/components/commons/adminlayout/adminheader/AdminHeader.styles.ts
+++ src/components/commons/adminlayout/adminheader/AdminHeader.styles.ts
@@ -3,6 +3,7 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div`
   width: 100%;
   position: sticky;
+  top: 0;
   z-index: 1;
 `;
 
@@ -15,6 +16,7 @@ export const Body = styled.div`
 export const IconWrapper = styled.div`
   display: flex;
   flex-direction: row;
+  justify-content: space-between;
 `;
 
 export const UserName = styled.label`
