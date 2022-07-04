aa5bb079dc793714cfca29dd5c5acd152b9dce4d
hailjeong <hailjeong96@gamil.com>
2022-07-04 19:03:52 +0900

    Admin Page Layout 초기

--- pages/adminpage/adminwrite/index.tsx
+++ pages/adminpage/adminwrite/index.tsx
@@ -1,5 +1,16 @@
+import { flexbox } from "@mui/system";
+import AdminHeaderPage from "../../../src/components/commons/adminlayout/adminheader/AdminHeader.presenter";
+import AdminSideBarUI from "../../../src/components/commons/adminlayout/adminsidebar/AdminSideBar.presenter";
 import AdminWriteUI from "../../../src/components/units/adminpage/adminwrite/AdminWrite.persenter";
 
 export default function AdminWritePage() {
-  return <AdminWriteUI />;
+  return (
+    <>
+      <AdminHeaderPage />
+      <div style={{ display: "flex", width: "500px" }}>
+        <AdminSideBarUI />
+        <AdminWriteUI />
+      </div>
+    </>
+  );
 }
---
+++ src/components/commons/adminlayout/adminsidebar/AdminSideBar.presenter.tsx
@@ -0,0 +1,7 @@
+export default function AdminSideBarUI() {
+  return (
+    <div>
+      <div>사이드바 입니다.</div>
+    </div>
+  );
+}
