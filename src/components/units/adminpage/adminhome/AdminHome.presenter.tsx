249003a405b07f603e0783fd13b6d6efa0d856a5
Bestlee94 <wagray84@gmail.com>
2022-07-05 15:41:01 +0900

    AdminHome 1차수정

--- src/components/units/adminpage/adminhome/AdminHome.presenter.tsx
+++ src/components/units/adminpage/adminhome/AdminHome.presenter.tsx
@@ -1,37 +1,78 @@
 import { Card } from "semantic-ui-react";
+import { Message } from "semantic-ui-react";
 
 export default function AdminHomeUI() {
   return (
-    <Card.Group>
-      <Card>
-        <Card.Content>
-          <Card.Header>회원 결제금액 합산</Card.Header>
-          <Card.Meta>transaction$</Card.Meta>
-          <Card.Description>총 240,000원</Card.Description>
-        </Card.Content>
-      </Card>
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <div
+        style={{
+          height: "100px",
+          marginTop: "100px",
+          display: "flex",
+        }}
+      >
+        <Message info>
+          <Message.Header>
+            서버 DB와 연결 성공! 데이터가 정상적으로 출력됩니다.
+          </Message.Header>
+          <p>다음 아래 목록에서 데이터를 확인해주세요!</p>
+        </Message>
+      </div>
 
-      <Card>
-        <Card.Content>
-          <Card.Header content="회원수" />
-          <Card.Meta content="Users" />
-          <Card.Description content="현재 가입된 회원수는 n명입니다." />
-        </Card.Content>
-      </Card>
+      <div
+        style={{
+          height: "100px",
+          marginTop: "100px",
+          display: "flex",
+        }}
+      >
+        <Message negative>
+          <Message.Header>
+            서버 DB와 연결 실패! 데이터가 정상적으로 출력되지 않습니다
+          </Message.Header>
+          <p>서버 연결상태를 확인해주세요!</p>
+        </Message>
+      </div>
 
-      <Card>
-        <Card.Content
-          header="예약내역"
-          meta="Reservation"
-          description="현재 예약진행중인 건은  n건입니다."
-        />
-      </Card>
+      <div
+        style={{
+          height: "150px",
+          marginTop: "100px",
+          display: "flex",
+        }}
+      >
+        <Card.Group>
+          <Card>
+            <Card.Content>
+              <Card.Header>결제금액 합산</Card.Header>
+              <Card.Meta>transaction$</Card.Meta>
+              <Card.Description>총 240,000원</Card.Description>
+            </Card.Content>
+          </Card>
 
-      <Card
-        header="문의내역"
-        meta="Inquiry details"
-        description="현재 미답변 문의내역은 n건입니다."
-      />
-    </Card.Group>
+          <Card>
+            <Card.Content>
+              <Card.Header content="회원수" />
+              <Card.Meta content="Users" />
+              <Card.Description content="현재 가입된 회원수는 n명입니다." />
+            </Card.Content>
+          </Card>
+
+          <Card>
+            <Card.Content
+              header="예약내역"
+              meta="Reservation"
+              description="현재 예약진행중인 건은  n건입니다."
+            />
+          </Card>
+
+          <Card
+            header="문의내역"
+            meta="Inquiry details"
+            description="현재 미답변 문의내역은 n건입니다."
+          />
+        </Card.Group>
+      </div>
+    </div>
   );
 }
