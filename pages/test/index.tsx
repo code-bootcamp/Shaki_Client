18b9bb083db91e04b1de88e00980e339f3c42793
KimSunWooo <rlatjsdn147@naver.com>
2022-07-06 17:29:49 +0900

    LangingAssist2

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -2,8 +2,7 @@ import * as S from "../../src/components/commons/landing/landing3/Landing3.style
 export default function Test() {
   return (
     <S.HTML>
-      <S.MapWrapper></S.MapWrapper>
-      <S.MapWrapper2></S.MapWrapper2>
+      <S.MapWrapper3></S.MapWrapper3>
     </S.HTML>
   );
 }
--- src/components/commons/landing/landing3/Landing3.styles.ts
+++ src/components/commons/landing/landing3/Landing3.styles.ts
@@ -7,18 +7,9 @@ export const HTML = styled.div`
   align-items: center;
 `;
 
-export const MapWrapper = styled.div`
+export const MapWrapper3 = styled.div`
   width: 100%;
-  height: 600px;
-  background-color: black;
-  border-top: 500px solid white;
-  border-right: 3144px solid transparent;
-`;
-
-export const MapWrapper2 = styled.div`
-  width: 100%;
-  height: 600px;
-  background-color: black;
-  border-bottom: 500px solid white;
-  border-left: 3144px solid transparent;
+  height: 500px;
+  background-color: pink;
+  transform: skewY(-10deg);
 `;
