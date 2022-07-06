881cf811051e4d7071216527787818042162a775
KimSunWooo <rlatjsdn147@naver.com>
2022-07-06 17:09:14 +0900

    Landing assist

--- src/components/commons/landing/landing3/Landing3.styles.ts
+++ src/components/commons/landing/landing3/Landing3.styles.ts
@@ -1,9 +1,24 @@
 import styled from "@emotion/styled";
-import { breakPoints } from "../../../../commons/styles/media";
+
+export const HTML = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export const MapWrapper = styled.div`
-  width: 1920px;
-  height: 1389.5px;
-  z-index: 3;
-  background-color: white;
+  width: 100%;
+  height: 600px;
+  background-color: black;
+  border-top: 500px solid white;
+  border-right: 3144px solid transparent;
+`;
+
+export const MapWrapper2 = styled.div`
+  width: 100%;
+  height: 600px;
+  background-color: black;
+  border-bottom: 500px solid white;
+  border-left: 3144px solid transparent;
 `;
