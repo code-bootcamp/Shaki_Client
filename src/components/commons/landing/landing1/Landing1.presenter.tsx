7ed8931c1ffa4de2a1829db6355fe4ad2b84aa03
Bestlee94 <wagray84@gmail.com>
2022-07-06 19:44:14 +0900

    LandinPage 미디어수정

--- src/components/commons/landing/landing1/Landing1.presenter.tsx
+++ src/components/commons/landing/landing1/Landing1.presenter.tsx
@@ -6,7 +6,7 @@ export default function Landing1UI() {
     <Ld.WrapperRoot>
       <Ld.Wrapper />
       <Ld.Icon src="/landing/landinglogo.png"></Ld.Icon>
-      <Ld.TitleHead>신개념 공유주방 플랫폼</Ld.TitleHead>
+      <Ld.TitleHead>신개념 공유주방</Ld.TitleHead>
       <Ld.Title steps={["Sha(re) Ki(tchen)", 1500]} wrapper="p"></Ld.Title>
     </Ld.WrapperRoot>
   );
