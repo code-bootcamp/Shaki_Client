3fc35c34d199fca0cd57fcf32c8ba6f790586175
Jae-hong-Lee <keash8091@gmail.com>
2022-07-06 10:25:50 +0900

    폴더구조 및 이름 변경(메인,디테일,렌더추가) , 카카오맵 공통폴더(API연결결X)

--- pages/adminpage/adminanswerwrite/index.tsx
+++ pages/adminpage/adminanswerwrite/index.tsx
@@ -1,7 +1,7 @@
 import styled from "@emotion/styled";
 import AdminHeaderPage from "../../../src/components/commons/adminlayout/adminheader/AdminHeader.presenter";
 import AdminSideBarUI from "../../../src/components/commons/adminlayout/adminsidebar/AdminSideBar.presenter";
-import AdminAnswerWriteUI from "../../../src/components/units/adminpage/adminanswerwrite/Admin.Answer.Write.presenter";
+import AdminAnswerWriteUI from "../../../src/components/units/adminpage/admin.answer.write/Admin.Answer.Write.presenter";
 
 const SideBar = styled.div`
   display: flex;
