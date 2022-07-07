92011768bcd81e4c97dc728d9dfa328271fa1929
Bestlee94 <wagray84@gmail.com>
2022-07-07 18:04:26 +0900

    Landing Page animaiton 구현

--- src/components/commons/landing/landing2/Landing2.presenter.tsx
+++ src/components/commons/landing/landing2/Landing2.presenter.tsx
@@ -1,20 +1,28 @@
+import useScrollFadeIn from "../../../../hooks/useScrollFadeIn";
 import * as Ld from "./Landing2.styles";
 
 export default function Landing2UI() {
+  const animatedItem = {
+    0: useScrollFadeIn("up", 1, 0),
+    1: useScrollFadeIn("up", 1, 0.1),
+    2: useScrollFadeIn("up", 1, 0.3),
+    3: useScrollFadeIn("up", 1, 0.4),
+  };
+
   return (
     <Ld.WrapperRoot>
       <Ld.Wrapper>
         <Ld.Rectangle></Ld.Rectangle>
-        <Ld.Header></Ld.Header>
-        <Ld.Contents></Ld.Contents>
+        <Ld.Header {...animatedItem[0]}></Ld.Header>
+        <Ld.Contents {...animatedItem[1]}></Ld.Contents>
         <Ld.MapWrapper>
           <Ld.MapBody></Ld.MapBody>
         </Ld.MapWrapper>
         <Ld.Box>
-          <Ld.BoxImage src="/landing/landingMent.svg" />
+          <Ld.BoxImage {...animatedItem[2]} src="/landing/landingMent.svg" />
         </Ld.Box>
       </Ld.Wrapper>
-      <Ld.Footer></Ld.Footer>
+      <Ld.Footer {...animatedItem[3]}></Ld.Footer>
     </Ld.WrapperRoot>
   );
 }
