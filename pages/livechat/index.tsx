af87f742d9c0c2e3d2a638f452fc2cb9e7053447
Bestlee94 <wagray84@gmail.com>
2022-07-12 13:17:29 +0900

    챗봇 초기구현

---
+++ pages/livechat/index.tsx
@@ -0,0 +1,5 @@
+import ChatBox from "../../src/components/units/livechat/chatbox/ChatBox.container";
+
+export default function Chat() {
+  return <ChatBox />;
+}
---
+++ src/components/units/livechat/chatbox/ChatBox.styles.ts
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  position: absolute;
+  bottom: 0rem;
+  width: 100%;
+`;
+
+export const Contents = styled.div`
+  display: flex;
+`;
+
+export const Input = styled.input`
+  flex: 1;
+  height: 2rem;
+`;
