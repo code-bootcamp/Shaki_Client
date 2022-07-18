198597ba040d3d7a1ba4409a346f8a92ddfe4155
KimSunWooo <rlatjsdn147@naver.com>
2022-07-18 16:30:57 +0900

    DeleteInquiry

--- src/components/units/inquiry/Inquiry.Container.tsx
+++
@@ -1,8 +0,0 @@
-import { useQuery } from "@apollo/client";
-import InquiryPresenter from "./Inquiry.Presenter";
-import { FETCH_QUESTIONS } from "./Inquiry.Query";
-
-export default function InquiryContainer() {
-  const { data } = useQuery(FETCH_QUESTIONS);
-  return <InquiryPresenter data={data} />;
-}
