1a07aab7c5328ac1979396885d6cc9afde83c547
KimSunWooo <rlatjsdn147@naver.com>
2022-07-31 00:51:21 +0900

    drag and drop edit

--- src/components/units/detailpage/detail.sidebar/drag&drop/style.ts
+++ src/components/units/detailpage/detail.sidebar/drag&drop/style.ts
@@ -12,29 +12,54 @@ export const Head = styled.div`
   margin-bottom: 30px;
 `;
 
-export const searchBar = styled.input`
-  margin-bottom: 20px;
+export const DragBox = styled.div`
+  display: flex;
+  flex-direction: row;
+  height: 150px;
+`;
+
+export const optionList = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const optionItem = styled.div`
+  width: 30%;
 `;
 
 export const DraggableItem = styled.div`
   background-color: #4167ee;
   width: fit-content;
+  height: 30px;
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 5px 5px;
   border-radius: 20px;
   font-size: 14px;
   font-weight: 600;
   color: white;
   padding: 5px 10px;
+  margin-left: 10px;
 `;
 
-export const Introduction = styled.div`
-  z-index: 1;
-  position: relative;
-  top: 250px;
-  left: 17%;
-  font-size: 30px;
-  opacity: 0.5;
+export const DragList = styled.div`
+  width: 100%;
+  padding: 10px 10px;
+  border: 1px solid black;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+export const DragZone = styled.div`
+  width: 30%;
+  background-color: #4167ee;
+  padding: 20px 10px;
+  border-top: 1px solid black;
+  border-bottom: 1px solid black;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 `;
